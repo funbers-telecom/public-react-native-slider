42d5ae54cdce6a86213d8536a61d2c9fde89bb15
Daniel Langesten <daniel@funbers.com>
2022-12-16 08:29:22 +0100

    Fix type properties issue

--- src/Slider.js
+++ src/Slider.js
@@ -9,7 +9,7 @@ import {
   Easing,
   I18nManager
 } from 'react-native';
-import { ViewPropTypes } from 'deprecated-react-native-prop-types';
+import { ViewPropTypes, ImagePropTypes } from 'deprecated-react-native-prop-types';
 import PropTypes from 'prop-types';
 
 const TRACK_SIZE = 4;
@@ -145,7 +145,7 @@ export default class Slider extends PureComponent {
     /**
      * Sets an image for the thumb.
      */
-    thumbImage: Image.propTypes.source,
+    thumbImage: ImagePropTypes.source,
 
     /**
      * Set this to true to visually see the thumb touch rect in green.
